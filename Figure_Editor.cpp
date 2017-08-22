233a07679c339f5a143859f2119072dcc308cc24
SaintForge <mr.spartan-117@yandex.ru>
2017-08-23 00:03:57 +0300

    added Figure Editor

---
+++ Figure_Editor.cpp
@@ -0,0 +1,219 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+#include "Figure_Editor.h"
+// #include "Figure_Manager.h"
+
+
+Figure_Editor::Figure_Editor(TTF_Font *& Font, Figure_Manager* FigureManager)
+{
+     printf("Figure_Editor::Figure_Editor()\n");
+     
+     if(!Font)
+     {
+	  printf("Passing null Font in Figure_Editor::Figure_Editor()\n");
+	  return;
+     }
+
+     this->Font = Font;
+     this->FigureManager = FigureManager;
+
+     InitTexture(NewFigureTexture, &NewFigureQuad, "+");
+     InitTexture(DelFigureTexture, &DelFigureQuad, "-");
+     InitTexture(RotateFigureTexture, "..\\data\\sprites\\restart_button2.png", &RotateFigureQuad);
+     InitTexture(FlipFigureTexture, "..\\data\\sprites\\flip_button.png", &FlipFigureQuad);
+     InitTexture(NewTypeTexture, &NewTypeQuad, ">");
+     InitTexture(NewFormTexture, &NewFormQuad, "<");
+
+
+     int width = Window_Info::get_width();
+     EditorBar.w = active_block_size * 6;
+     EditorBar.h = active_block_size;
+     EditorBar.x = 0;
+     EditorBar.y = active_block_size;
+}
+
+Figure_Editor::~Figure_Editor()
+{
+     printf("~Figure_Editor::Figure_Editor()\n");
+     if(NewFigureTexture) SDL_DestroyTexture(NewFigureTexture);
+     if(DelFigureTexture) SDL_DestroyTexture(DelFigureTexture);
+     if(NewTypeTexture) SDL_DestroyTexture(NewTypeTexture);
+     if(NewFormTexture) SDL_DestroyTexture(NewFormTexture);
+     if(RotateFigureTexture) SDL_DestroyTexture(RotateFigureTexture);
+     if(FlipFigureTexture) SDL_DestroyTexture(FlipFigureTexture);
+}
+
+void Figure_Editor::InitTexture(SDL_Texture*& Texture, const char* Path, SDL_Rect* Quad)
+{
+     if(!Path) return;
+     if(Texture)
+     {
+	  SDL_DestroyTexture(Texture);
+     }
+
+     SDL_Renderer *RenderScreen = Window_Info::get_renderer();
+     SDL_Surface* tmp_surface = IMG_Load(Path);
+     if(!tmp_surface)
+     {
+	  printf("Failed to init SDL_Surface at Figure_Editor::InitTexture() - %s\n", IMG_GetError());
+	  return;
+     }
+
+     Quad->w = active_block_size;
+     Quad->h = active_block_size;
+
+     Texture = SDL_CreateTextureFromSurface(RenderScreen, tmp_surface);
+     if(!Texture)
+     {
+	  printf("Failed to init SDL_Texture at Figure_Editor::InitTexture = %s", SDL_GetError() );
+     }
+
+     SDL_FreeSurface(tmp_surface);
+}
+
+void Figure_Editor::InitTexture(SDL_Texture*& Texture, SDL_Rect* Quad, const char* Text)
+{
+     if(!Font) return;
+     if(!Text) return;
+     if(Texture)
+     {
+	  SDL_DestroyTexture(Texture);
+     }
+     
+     SDL_Renderer* RenderScreen = Window_Info::get_renderer();
+
+     SDL_Surface* tmp_surface = TTF_RenderUTF8_Blended(Font,  Text,  {0, 0, 0});
+     if(!tmp_surface)
+     {
+	  printf("Failed to init SDL_Surface at Figure_Editor::InitTexture = %s", TTF_GetError() );
+	  return;
+     }
+
+     Quad->w = tmp_surface->w;
+     Quad->h = tmp_surface->h; 
+
+     Texture = SDL_CreateTextureFromSurface(RenderScreen,  tmp_surface);
+     if(!Texture)
+     {
+	  printf("Failed to init SDL_Texture at Figure_Editor::InitTexture = %s", SDL_GetError() );
+     }
+
+     SDL_FreeSurface(tmp_surface);
+}
+
+static
+bool ProcessMouseInput(SDL_Rect* TargetQuad, int x, int y)
+{
+     if(x < TargetQuad->x) return false ;
+     else if(x > TargetQuad->x + TargetQuad->w) return false ;
+     else if(y < TargetQuad->y) return false ;
+     else if(y > TargetQuad->y + TargetQuad->h) return false;
+     else return true;
+}
+
+void Figure_Editor::HandleEvent(SDL_Event* event)
+{
+     if(event->type == SDL_MOUSEBUTTONDOWN && event->button.button == SDL_BUTTON_LEFT)
+     {
+	  if(!MousePressed)
+	  {
+	       MousePressed = true;
+	       printf("MousePressed = true\n");
+
+	       int x_button = event->button.x;
+	       int y_button = event->button.y;
+
+	       SDL_Rect TargetPosition = {};
+	       TargetPosition.x = EditorBar.x;
+	       TargetPosition.y = EditorBar.y;
+	       TargetPosition.w = active_block_size;
+	       TargetPosition.h = active_block_size;
+
+	       if(ProcessMouseInput(&TargetPosition, x_button, y_button))
+	       {
+		    printf("New Figure hit!\n");
+		    FigureManager->add_figure(O_figure, classic);
+	       }
+
+	       TargetPosition.x += TargetPosition.w;
+	       if(ProcessMouseInput(&TargetPosition, x_button, y_button))
+	       {
+		    printf("Delete Figure hit!\n");
+		    FigureManager->delete_last_figure();
+	       }
+
+	       TargetPosition.x += TargetPosition.w;
+	       if(ProcessMouseInput(&TargetPosition, x_button, y_button))
+	       {
+		    printf("changing form!\n");
+		    FigureManager->change_figure_form(FigureManager->get_figure_amount() - 1);
+	       }
+
+	       TargetPosition.x += TargetPosition.w;
+	       if(ProcessMouseInput(&TargetPosition, x_button, y_button))
+	       {
+		    printf("changing type!\n");
+		    FigureManager->change_figure_type(FigureManager->get_figure_amount() - 1);    
+	       }
+
+	       TargetPosition.x += TargetPosition.w;
+	       if(ProcessMouseInput(&TargetPosition, x_button, y_button))
+	       {
+		    printf("rotating button!\n");
+		    FigureManager->change_figure_angle(FigureManager->get_figure_amount() - 1);
+	       }
+
+	       TargetPosition.x += TargetPosition.w;
+	       if(ProcessMouseInput(&TargetPosition, x_button, y_button))
+	       {
+		    
+	       }
+	  }
+     }
+     else if(event->type == SDL_MOUSEBUTTONUP && event->button.button == SDL_BUTTON_LEFT)
+     {
+	  if(MousePressed)
+	  {
+	       MousePressed = false;
+	       printf("MousePresed = false\n");
+	  }
+     }
+}
+
+static
+void RenderUIElement(SDL_Texture*& Texture, SDL_Rect* TextureRect, SDL_Rect* UiRect)
+{
+     SDL_Renderer* RenderScreen = Window_Info::get_renderer();
+
+     SDL_RenderDrawRect(RenderScreen, UiRect);
+     
+     TextureRect->x = (UiRect->x + UiRect->w / 2 ) - (TextureRect->w / 2);
+     TextureRect->y = (UiRect->y + UiRect->w / 2 ) - (TextureRect->h / 2);
+     UiRect->x += UiRect->h;
+
+     SDL_RenderCopy(RenderScreen, Texture, 0, TextureRect );
+}
+
+void Figure_Editor::RenderEditor()
+{
+     SDL_Renderer* RenderScreen = Window_Info::get_renderer();
+
+     SDL_Rect TargetPosition = {EditorBar.x, EditorBar.y,
+				active_block_size, active_block_size};
+
+     SDL_SetRenderDrawColor(RenderScreen, 0, 255, 0, 255);
+     SDL_RenderFillRect(RenderScreen, &EditorBar);
+     SDL_SetRenderDrawColor(RenderScreen, 0, 0, 0, 255);
+     SDL_RenderDrawRect(RenderScreen, &EditorBar);
+
+     RenderUIElement(NewFigureTexture, &NewFigureQuad, &TargetPosition);
+     RenderUIElement(DelFigureTexture, &DelFigureQuad, &TargetPosition);
+     RenderUIElement(NewFormTexture, &NewFormQuad, &TargetPosition);
+     RenderUIElement(NewTypeTexture, &NewTypeQuad, &TargetPosition);
+     RenderUIElement(RotateFigureTexture, &RotateFigureQuad, &TargetPosition);
+     RenderUIElement(FlipFigureTexture, &FlipFigureQuad, &TargetPosition);
+
+
+}
---
+++ Figure_Editor.h
@@ -0,0 +1,41 @@
+#ifndef FIGURE_EDITOR_H
+#define FIGURE_EDITOR_H
+
+#include <SDL.h>
+#include "Figure.h"
+#include "Figure_Manager.h"
+
+struct Figure_Editor
+{
+     TTF_Font *Font;
+     SDL_Rect EditorBar;
+
+     SDL_Rect NewFigureQuad;
+     SDL_Rect DelFigureQuad;
+     SDL_Rect NewTypeQuad;
+     SDL_Rect NewFormQuad;
+     SDL_Rect RotateFigureQuad;
+     SDL_Rect FlipFigureQuad;
+
+     SDL_Texture *NewFigureTexture    = NULL;
+     SDL_Texture *DelFigureTexture    = NULL;
+     SDL_Texture *NewTypeTexture      = NULL;
+     SDL_Texture *NewFormTexture      = NULL;
+     SDL_Texture *RotateFigureTexture = NULL;
+     SDL_Texture *FlipFigureTexture   = NULL;
+     
+     Figure_Manager* FigureManager;
+
+     bool FigureIsSelected = false;
+     bool MousePressed     = false;
+
+     Figure_Editor(TTF_Font*& Font, Figure_Manager* FigureManager);
+     ~Figure_Editor();
+     
+     void InitTexture(SDL_Texture*& Texture, SDL_Rect* Target, const char* Text);
+     void InitTexture(SDL_Texture*& Texture, const char* Path, SDL_Rect *Target);
+     void HandleEvent(SDL_Event *event);
+     void RenderEditor();
+};
+
+#endif
